9b7a0443c21a2905e81a2c18a417f1ab808409dc
ksamrat224 <ksamrat224@gmail.com>
2025-03-02 21:36:17 +0545

    trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { API_OPTIONS } from '../Utils/constants'
 
-const VideoBackground = () => {
+const VideoBackground = ({movieId}) => {
+  //fetch trailer video
+  const getMovieVideos = async()=> {
+    const data = await fetch('https://api.themoviedb.org/3/movie/950396/videos?language=en-US', API_OPTIONS);
+    const json = await data.json();
+   console.log(json);
+
+   const filterData= json.results.filter((video)=>video.type === "Trailer");
+   const trailer =filterData.length? filterData[0]:json.results[0];
+   console.log(trailer);
+};
+useEffect(()=>{
+getMovieVideos();
+},[]);
   return (
-    <div>VideoBackground</div>
+    <div><iframe
+     width="560" 
+     height="315" 
+     src={`https://www.youtube.com/embed/${trailer?.key}?si=s7y2h1X7sM6jlhLU`}
+     title="YouTube video player"
+     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe></div>
   )
 }
 
