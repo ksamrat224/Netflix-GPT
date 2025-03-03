c3345e181ff5a83cdcdaa577715a34983bea8292
ksamrat224 <ksamrat224@gmail.com>
2025-03-03 16:12:55 +0545

    video background

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,25 +1,17 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../Utils/constants'
+import React from 'react'
+import { useSelector } from 'react-redux';
+import useMovieTrailer from '../hooks/useMovieTrailer';
+
 
 const VideoBackground = ({movieId}) => {
-  //fetch trailer video
-  const getMovieVideos = async()=> {
-    const data = await fetch('https://api.themoviedb.org/3/movie/950396/videos?language=en-US', API_OPTIONS);
-    const json = await data.json();
-   console.log(json);
-
-   const filterData= json.results.filter((video)=>video.type === "Trailer");
-   const trailer =filterData.length? filterData[0]:json.results[0];
-   console.log(trailer);
-};
-useEffect(()=>{
-getMovieVideos();
-},[]);
+  const trailerVideo = useSelector(store=>store.movies?.trailerVideo);
+  useMovieTrailer(movieId);
+  
   return (
-    <div><iframe
-     width="560" 
-     height="315" 
-     src={`https://www.youtube.com/embed/${trailer?.key}?si=s7y2h1X7sM6jlhLU`}
+    <div className=' w-screen'>
+    <iframe
+    className='w-screen aspect-video'
+     src={`https://www.youtube.com/embed/${trailerVideo?.key}?si=s7y2h1X7sM6jlhLU`}
      title="YouTube video player"
      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe></div>
   )
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className='pt-36 px-12'>
+    <div className='w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black'>
        <h1 className='text-6xl font-extrabold drop-shadow-lg'>{title}</h1> 
-       <p className="text-lg text-blue-950 mt-4 max-w-2xl drop-shadow-md">{overview}</p>
+       <p className="text-lg text-white mt-4 max-w-2xl drop-shadow-md">{overview}</p>
        <div className='flex space-x-4 mt-6'>
         <button className='bg-gray-400 text-white p-4 px-12  flex items-center rounded-lg text-xl bg-opacity-50'>▶️Play</button>
         <button className='bg-gray-400 text-white p-4 px-12  flex items-center rounded-lg text-xl bg-opacity-50'>❕More Info</button>
