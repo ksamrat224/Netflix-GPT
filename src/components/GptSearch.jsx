4202c83aa8e65d636968ba418bfcaa19674a0767
ksamrat224 <ksamrat224@gmail.com>
2025-03-06 20:52:30 +0545

    search box

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import GptSearchBar from './GptSearchBar'
 import GptMovieSuggestions from './GptMovieSuggestions'
+import { BODY_IMG } from '../Utils/constants'
 
 const GptSearch = () => {
   return (
     <div>
+      <div className='absolute -z-10 '>
+              <img
+                src={BODY_IMG}
+                alt="Body-Image"
+              />
+              </div>
       <GptSearchBar/>
       <GptMovieSuggestions/>
     </div>
--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const GptSearchBar = () => {
   return (
-    <div>GptSearchBar</div>
+    <div className='pt-[10%] flex justify-center'>
+        <form className=' bg-black w-1/2 grid grid-cols-12 rounded-lg'>
+
+         <input type="text" className='p-4 m-4 rounded-md col-span-9' placeholder='what would you like to watch today?'/>
+
+         <button className='py-2 px-4 m-4 bg-red-800 text-white rounded-lg col-span-3'>Search</button>
+        </form>
+    </div>
   )
 }
 
